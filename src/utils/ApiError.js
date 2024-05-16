447c78bd1534b87ff342bfa2a03f633d25909260
samir 9187 <samirkunwar9187@gmail.com>
2024-05-16 20:40:52 +0530

    #comment added

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -21,3 +21,5 @@ class ApiError extends Error {
 }
 
 export { ApiError };
+
+// The purpose of the provided JavaScript code is to define a custom error class called ApiError. This class extends the built-in Error class and is designed to handle API-related errors in a structured and consistent manner.
