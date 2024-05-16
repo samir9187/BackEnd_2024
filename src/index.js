4f63f5a1593b49015dcd80ddffb9e772ce4d54b5
samir 9187 <samirkunwar9187@gmail.com>
2024-05-16 19:37:06 +0530

    changes in uti folder

--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ dotenv.config({
   path: "env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.port || 8000, () => {
+      console.log(`Server is running at port : ${process.env.port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!!!", err);
+  });
 
 /*
 (async () => {
